a79e7e84dcbab166cd0094521803bcd983f23c3e
hemantsachdeva <hksachdeva007@gmail.com>
2020-11-27 00:53:05 +0530

    laurel_sprout: Add prebuilt GCamGo *nuked Snap camera *experience better quality clicks now
    Signed-off-by: hemantsachdeva <hksachdeva007@gmail.com>

--- device.mk
+++ device.mk
@@ -159,7 +159,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libdng_sdk.vendor \
-    Snap
+    GCamGo
 
 # Context Hub
 PRODUCT_PACKAGES += \
